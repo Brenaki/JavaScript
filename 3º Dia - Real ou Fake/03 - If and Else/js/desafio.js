56b52b6e7db566698b38182e264c394677218729
Victor Angelo Legat Cerqueira <91611155+Brenaki@users.noreply.github.com>
2021-11-04 22:47:59 -0300

    Update - Desafio, Folder"If and Else"
    Corrigi o meu erro, ao invés de ser src é href
    Desculpa ter feito essa bobagem deixei passar na hora que eu estava programando

--- 3º Dia - Real ou Fake/03 - If and Else/js/desafio.js
+++ 3º Dia - Real ou Fake/03 - If and Else/js/desafio.js
@@ -13,23 +13,23 @@ if (resposta >= 6){
     switch (resposta){
 
         case 6:
-            link = '<a src="https://www.youtube.com/watch?v=6ShWtI6geWw"><h1>Clique em mim!!</h1></a>';
+            link = '<a href="https://www.youtube.com/watch?v=6ShWtI6geWw"><h1>Clique em mim!!</h1></a>';
             break;
 
         case 7:
-            link = '<a src="https://www.youtube.com/watch?v=lqtPC2q4we0"><h1>Clique em mim!!</h1></a>';
+            link = '<a href="https://www.youtube.com/watch?v=lqtPC2q4we0"><h1>Clique em mim!!</h1></a>';
             break;
 
         case 8:
-            link = '<a src="https://www.youtube.com/watch?v=KXXs0nWlfcI"><h1>Clique em mim!!</h1></a>';
+            link = '<a href="https://www.youtube.com/watch?v=KXXs0nWlfcI"><h1>Clique em mim!!</h1></a>';
             break;
 
         case 9:
-            link = '<a src="https://www.youtube.com/watch?v=fPl-20XpSDM"><h1>Clique em mim!!</h1></a>';
+            link = '<a href="https://www.youtube.com/watch?v=fPl-20XpSDM"><h1>Clique em mim!!</h1></a>';
             break;
         
         default:
-            link = '<a src="https://www.youtube.com/watch?v=e1JjkZzs_s0"><h1>Clique em mim!!</h1></a>';
+            link = '<a href="https://www.youtube.com/watch?v=e1JjkZzs_s0"><h1>Clique em mim!!</h1></a>';
             break;
     }
     document.write(link);
@@ -38,6 +38,6 @@ if (resposta >= 6){
 else {
 
     document.write('<h1>Se jogo um número muito baixo</h1>');
-    document.write('<a src="https://www.youtube.com/watch?v=IQ1p_993LC4"><h2>Mas tá aqui seu link</h2></a>');
+    document.write('<a href="https://www.youtube.com/watch?v=IQ1p_993LC4"><h2>Mas tá aqui seu link</h2></a>');
 
 }
